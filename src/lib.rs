59f3ecb1d78db910bf26360ceff3aa9ff4146e96
mzumi <masazumi.m@gmail.com>
2017-04-01 13:19:34 +0900

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,69 @@
+extern crate csv;
+extern crate pbr;
+extern crate regex;
+
+use regex::*;
+use std::fs;
+
+use pbr::{ProgressBar, Units};
+
+#[derive(Debug)]
+pub struct CSVTools {}
+
+impl CSVTools {
+    pub fn convert(from: String, to: String, source: String, output: String) {
+        let converter = Convertor::new(from.to_owned(),
+                                       to.to_owned(),
+                                       source.to_owned(),
+                                       output.to_owned());
+        converter.run();
+    }
+}
+
+#[derive(Debug)]
+struct Convertor {
+    regex: Result<Regex, Error>,
+    target: String,
+    source: String,
+    output: String,
+}
+
+impl Convertor {
+    pub fn new(from: String, to: String, source: String, output: String) -> Self {
+        Convertor {
+            regex: Regex::new(from.as_str()),
+            target: to,
+            source: source,
+            output: output,
+        }
+    }
+
+    fn run(&self) {
+        if let Ok(ref re) = self.regex {
+            let mut writer = csv::Writer::from_file(&self.output).unwrap();
+
+            let mut reader = csv::Reader::from_file(&self.source).unwrap();
+
+            let n_bytes = fs::metadata(&self.source).unwrap().len() as usize;
+            let mut pb = ProgressBar::new(n_bytes as u64);
+            pb.set_units(Units::Bytes);
+
+            let mut copied_size: usize = 0;
+            for h in reader.headers() {
+                copied_size += h.iter().fold(0, |sum, s| sum + s.capacity());
+                let _ = writer.write(h.into_iter());
+                pb.set(copied_size as u64);
+            }
+
+            for row in reader.records().map(|r| r.unwrap()) {
+                copied_size += row.iter().fold(0, |sum, s| sum + s.capacity());
+
+                let after = row.iter()
+                    .map(|r| re.replace_all(r.as_str(), self.target.as_str()));
+                let _ = writer.write(after.into_iter());
+                pb.set(copied_size as u64);
+            }
+            pb.finish();
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,44 @@
+extern crate csv_tools;
+extern crate clap;
+
+use csv_tools::*;
+use clap::*;
+
+fn main() {
+    let matches = App::new("rc")
+        .version("0.1")
+        .author("mzumi")
+        .about("CSV Tools")
+        .subcommand(SubCommand::with_name("convert")
+            .about("controls testing features")
+            .version("0.1")
+            .arg(Arg::with_name("from")
+                .help("Sets the string to be converted")
+                .required(true)
+                .index(1))
+            .arg(Arg::with_name("to")
+                .help("Sets the converted string")
+                .required(true)
+                .index(2))
+            .arg(Arg::with_name("INPUT")
+                .help("Sets the input file to use")
+                .required(true)
+                .index(3))
+            .arg(Arg::with_name("OUTPUT")
+                .help("Sets the output file to use")
+                .required(true)
+                .index(4)))
+        .get_matches();
+
+    if let Some(matches) = matches.subcommand_matches("convert") {
+        let from = matches.value_of("from").unwrap();
+        let to = matches.value_of("to").unwrap();
+        let input = matches.value_of("INPUT").unwrap();
+        let output = matches.value_of("OUTPUT").unwrap();
+
+        CSVTools::convert(from.to_owned(),
+                          to.to_owned(),
+                          input.to_owned(),
+                          output.to_owned());
+    }
+}
